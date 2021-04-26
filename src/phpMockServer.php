6f18cda7f88e52fe11074d306eab101a6c2512d8
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2021-04-26 13:03:55 +0200

    AndreyT code review

--- src/phpMockServer.php
+++ src/phpMockServer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace dagsta\pms;
 
 require_once "customCallbackInterface.php";
@@ -9,116 +10,133 @@ use Symfony\Component\HttpFoundation\Response;
 
 class phpMockServer
 {
+    const MOCK_KEY_RULES = 'rules';
+    const MOCK_KEY_CUSTOM_CALLBACK = 'customCallback';
+    const MOCK_KEY_LATENCY = 'latency';
+    /* I would rename it to `headers` */
+    const MOCK_KEY_HEADER = 'header';
+    const MOCK_KEY_HTTPCODE = 'httpcode';
+    const MOCK_KEY_BODY = 'body';
     private $request;
     private $response;
     private const FETCHCALL = 1;
     private const MOCKCALL = 2;
 
     private $configBasePath;
-    function __construct($configBasePath = null) {
-        $this->request = Request::createFromGlobals();
-        $this->response= new Response();
-        if(!is_null($configBasePath)){
-            $this->configBasePath = $configBasePath;
-        }
-        else{
-            $this->configBasePath = __DIR__.'/../mocks';
-        }
 
+    function __construct(string $configBasePath)
+    {
+        $this->request = Request::createFromGlobals();
+        $this->response = new Response();
+        $this->configBasePath = $configBasePath;
     }
-    function run(): void {
-        if($this->determineRequestType() == self::FETCHCALL){
+
+    public function run(): void
+    {
+        if ($this->determineRequestType() == self::FETCHCALL) {
             $this->performCallFetch();
-        } else{
-          $this->performMockRequest();
+        } else {
+            $this->performMockRequest();
         }
         $this->response->send();
     }
 
-    function performCallFetch(): void{
+    protected function performCallFetch(): void
+    {
         $this->readMockedRequest();
     }
 
-    private function getConfigPath(): string{
+    private function getConfigPath(): string
+    {
         $parts = explode("/", $this->getPath());
         if ($parts[count($parts) - 1] == "") {
             unset($parts[count($parts) - 1]);
         }
         $path = implode("/", $parts);
-        return $this->configBasePath. DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . "mock.json";
+        return $this->configBasePath . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . "mock.json";
     }
 
-    function performMockRequest(): bool{
+    protected function performMockRequest(): bool
+    {
         $conf = $this->selectMatchingConfig();
-        if($conf === false){
+        if ($conf === false) {
             $this->response->setContent('No Mock found for this endpoint');
             $this->response->setStatusCode(404);
             return false;
         }
-        if (isset($conf['customCallback'])) {
-            $classFilePath = dirname($this->getConfigPath()) . DIRECTORY_SEPARATOR . $conf['customCallback'] . ".php";
+
+        if (isset($conf[self::MOCK_KEY_CUSTOM_CALLBACK])) {
+            $classFilePath = dirname($this->getConfigPath()) . DIRECTORY_SEPARATOR . $conf[self::MOCK_KEY_CUSTOM_CALLBACK] . ".php";
             try {
                 if (file_exists($classFilePath)) {
                     require_once $classFilePath;
-                    if (class_exists($conf['customCallback'])) {
-                        $controller = new $conf['customCallback']();
-                        $adddata = $controller->run($this->request, $this->response);
+                    if (class_exists($conf[self::MOCK_KEY_CUSTOM_CALLBACK])) {
+                        $controller = new $conf[self::MOCK_KEY_CUSTOM_CALLBACK]();
+                        if ($controller instanceof customCallbackInterface) {
+                            $adddata = $controller->run($this->request, $this->response);
+                        }
                     }
                 }
-            }
-            catch(\Exception $e){
-                $this->response->setContent("Failed to call customCallbock:".$conf['customCallback'].PHP_EOL.$e->getMessage());
+
+                /* we need as much validation as possible - along the way or when mock is being chosen */
+                if (!isset($controller)) {
+                    throw new \Exception('No callback found.');
+                }
+            } catch (\Exception $e) {
+                $this->response->setContent("Failed to call customCallbock:" . $conf[self::MOCK_KEY_CUSTOM_CALLBACK] . PHP_EOL . $e->getMessage());
                 return false;
             }
-        } else {
-            if (isset($conf['latency'])) {
-                sleep($conf['latency']);
-            }
-            if (isset($conf['header'])) {
-                $this->response->headers->add($conf['header']);
-            }
-            if(isset($conf['httpcode'])){
-                $this->response->setStatusCode($conf['httpcode']);
-            }
-            if(is_array($conf['body']))
-            {
-                $this->response->headers->set('Content-Type', 'application/json');
-                $this->response->setContent(json_encode($conf['body']));
-            }
-            else{
-                $this->response->setContent($conf['body']);
-            }
-            $adddata = [];
+
+            $this->storeMockRequest($adddata);
+            return true;
         }
-        $this->storeMockRequest($adddata);
+
+        if (isset($conf[self::MOCK_KEY_LATENCY])) {
+            sleep($conf[self::MOCK_KEY_LATENCY]);
+        }
+        if (isset($conf[self::MOCK_KEY_HEADER])) {
+            $this->response->headers->add($conf[self::MOCK_KEY_HEADER]);
+        }
+        if (isset($conf[self::MOCK_KEY_HTTPCODE])) {
+            $this->response->setStatusCode($conf[self::MOCK_KEY_HTTPCODE]);
+        }
+        if (is_array($conf[self::MOCK_KEY_BODY])) {
+            /* I would expect this to be moved to headers inside mock, and add additional key `bodyIsJson` by which encode the body. */
+            $this->response->headers->set('Content-Type', 'application/json');
+            $this->response->setContent(json_encode($conf[self::MOCK_KEY_BODY]));
+        } else {
+            $this->response->setContent($conf[self::MOCK_KEY_BODY]);
+        }
+
+        $this->storeMockRequest();
         return true;
     }
 
-    private function checkRules($rules): bool{
-        foreach($rules as $type => $ruleset) {
-            $classname = __NAMESPACE__.'\\rules\\' . strtolower($type) . "RuleImplementation";
-            if (class_exists($classname)){
+    private function checkRules($rules): bool
+    {
+        foreach ($rules as $type => $ruleset) {
+            $classname = __NAMESPACE__ . '\\rules\\' . strtolower($type) . "RuleImplementation";
+            if (class_exists($classname) && new $classname instanceof ruleImplementationInterface) {
                 if (!$classname::check($this->request, $ruleset)) {
                     return false;
                 }
-            }
-            else{
-                //ToDo Display Error?!?
+            } else {
+                //ToDo Display Error?!? /* I'd say - yes, error is required here */
                 return false;
             }
 
         }
         return true;
     }
-    
-    function selectMatchingConfig(){
+
+    protected function selectMatchingConfig()
+    {
         $config = $this->getMockConfig();
         $methode = $this->getMethode();
-        if(isset($config[$methode]))
-        {
-            foreach($config[$methode] as $key => $mock){
-                if(!isset($mock['rules']) OR $this->checkRules($mock['rules']))
-                {
+        if (isset($config[$methode])) {
+            foreach ($config[$methode] as $key => $mock) {
+                /* I think we have to order mocks by presence of rules and take not the first without rules, but rather the one with matching rule */
+                if (!isset($mock[self::MOCK_KEY_RULES]) or $this->checkRules($mock[self::MOCK_KEY_RULES])) {
                     return $mock;
                 }
             }
@@ -126,41 +144,61 @@ class phpMockServer
         return false;
     }
 
-    private function determineRequestType(): int {
+    /**
+     * could be moved to the RequestDispatcher class
+     *
+     * RequestDispatcher
+     * accepts request (or takes from globals)
+     * returns object with:
+     * - request type
+     * - request method
+     * - request path
+     */
+    private function determineRequestType(): int
+    {
         $parts = explode("/", $this->request->getPathInfo());
-        if(count($parts) > 3 && $parts[1] == "getCallPayload") {
+
+        if (count($parts) > 3 && $parts[1] == "getCallPayload") {
             return self::FETCHCALL;
-        }else{
-            return self::MOCKCALL;
         }
+
+        return self::MOCKCALL;
     }
 
-    private function getMethode() {
-        if($this->determineRequestType() == self::FETCHCALL){
+    /**
+     * should be moved to the RequestDispatcher class
+     */
+    private function getMethode()
+    {
+        if ($this->determineRequestType() == self::FETCHCALL) {
             $parts = explode("/", $this->request->getPathInfo());
             return strtoupper($parts[2]);
-        }
-        else{
+        } else {
             return $this->request->getMethod();
         }
     }
 
-    private function getPath() {
+    /**
+     * should be moved to the RequestDispatcher class
+     */
+    private function getPath()
+    {
         $parts = explode("/", $this->request->getPathInfo());
-        if($this->determineRequestType() == self::FETCHCALL){
-            if($parts[count($parts)-1] == ""){
-                unset($parts[count($parts)-1]);
+        if ($this->determineRequestType() == self::FETCHCALL) {
+            if ($parts[count($parts) - 1] == "") {
+                unset($parts[count($parts) - 1]);
             }
             unset($parts[1]);
             unset($parts[2]);
-            return implode("/",$parts);
+            return implode("/", $parts);
         }
-        return substr($this->request->getPathInfo(),1);
+        return substr($this->request->getPathInfo(), 1);
     }
 
-    private function storeMockRequest($adddata = []){
-        $datapath = __DIR__.'/../data/'.$this->request->getMethod().DIRECTORY_SEPARATOR
-            .$this->getPath();
+    private function storeMockRequest($adddata = [])
+    {
+        $datapath = __DIR__ . '/../data/' . $this->request->getMethod() . DIRECTORY_SEPARATOR
+            . $this->getPath();
         if (!file_exists(dirname($datapath))) {
             mkdir(dirname($datapath), 0700, true);
         }
@@ -168,22 +206,24 @@ class phpMockServer
         file_put_contents($datapath, json_encode($data));
     }
 
-    private function readMockedRequest(){
+    private function readMockedRequest()
+    {
         $configpath = $this->getConfigPath();
-        if(!file_exists($configpath)){
+
+        if (!file_exists($configpath)) {
             $this->response->setContent('No Mockconfiguration found for this endpoint');
             $this->response->setStatusCode(404);
             return;
         }
-        $datapath = __DIR__.'/../data/'.$this->getMethode()
-            .$this->getPath();
+        $datapath = __DIR__ . '/../data/' . $this->getMethode()
+            . $this->getPath();
         $timeout = $this->request->headers->get("X-timeout", 60);
         $count = 0;
-        while($count < $timeout && !file_exists($datapath)){
+        while ($count < $timeout && !file_exists($datapath)) {
             sleep(1);
             $count++;
         }
-        if($count == $timeout) {
+        if ($count == $timeout) {
             $this->response->setContent('Timeout');
             $this->response->setStatusCode(500);
         }
@@ -192,14 +232,13 @@ class phpMockServer
         unlink($datapath);
     }
 
-    private function getMockConfig(){
+    private function getMockConfig()
+    {
         $configpath = $this->getConfigPath();
-        if (!file_exists($configpath)) {
-            return false;
+        if (file_exists($configpath)) {
+            return json_decode(file_get_contents($configpath), true);
         }
-        else{
-            return json_decode(file_get_contents($configpath),true);
-        }
+        return [];
     }
 
 
